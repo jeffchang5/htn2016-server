53a6ceb3fe30fe1b1f543759ab14eab546d616df
Jozsef Vass <jvass@twilio.com>
2014-11-06 21:40:26 -0800

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+Sample Heroku Deploy App
+===
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
---
+++ server.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask, request
+from twilio.util import TwilioCapability
+import twilio.twiml
+
+ACCOUNT_SID = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+AUTH_TOKEN = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
+APP_SID = 'APabe7650f654fc34655fc81ae71caa3ff'
+CALLER_ID = '+12345678901'
+CLIENT = 'jenny'
+
+app = Flask(__name__)
+
+@app.route('/token')
+def token():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+  app_sid = os.environ.get("APP_SID", APP_SID)
+
+  capability = TwilioCapability(account_sid, auth_token)
+
+  # This allows outgoing connections to application
+  if request.values.get('allowOutgoing') != 'false':
+     capability.allow_client_outgoing(app_sid)
+
+  # This allows incoming connections to client (if specified)
+  client = request.values.get('client')
+  if client != None:
+    capability.allow_client_incoming(client)
+
+  # This returns a token to use with Twilio based on the account and capabilities defined above
+  return capability.generate()
+
+@app.route('/call', methods=['GET', 'POST'])
+def call():
+  """ This method routes calls from/to client                  """
+  """ Rules: 1. From can be either client:name or PSTN number  """
+  """        2. To value specifies target. When call is coming """
+  """           from PSTN, To value is ignored and call is     """
+  """           routed to client                               """
+  resp = twilio.twiml.Response()
+  from_value = request.values.get('From')
+  to = request.values.get('To')
+  if not (from_value and to):
+    return str(resp.say("Invalid request"))
+  from_client = from_value.startswith('client')
+  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+  if not from_client:
+    # PSTN -> client
+    resp.dial(callerId=from_value).client(CLIENT)
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
+  return str(resp)
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
