fe9679c2ec2bbb933da483ea308820e20a090279
Jeffrey Chang <jeffreychang@v1021-wn-82-212.campus-dynamic.uwaterloo.ca>
2016-09-17 20:14:19 -0400

    added some stuff

--- server.py
+++ server.py
@@ -1,71 +1,30 @@
 import os
 from flask import Flask, request
-from twilio.util import TwilioCapability
-import twilio.twiml
-
+from twilio.rest import TwilioRestClient
 # Account Sid and Auth Token can be found in your account dashboard
-ACCOUNT_SID = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-AUTH_TOKEN = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
+ACCOUNT_SID = 'ACb0cacf43e157a1ec57ca591fa3fdbcf4'
+AUTH_TOKEN = '72ac923817baa54a6a8be2b30c334335'
 
-# TwiML app outgoing connections will use
-APP_SID = 'APZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
 
-CALLER_ID = '+12345678901'
-CLIENT = 'jenny'
+account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+client = TwilioRestClient(account_sid, auth_token)
+
 
 app = Flask(__name__)
+@app.route('/text/<number>', methods=['POST'])
+def text(number):
+  content = request.get_json(silent=True)
+  print(content)
+  message = client.messages.create(to=number, from_="+12267740479",
+                                   body="Hello there!")
 
-@app.route('/token')
-def token():
-  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
-  app_sid = os.environ.get("APP_SID", APP_SID)
 
-  capability = TwilioCapability(account_sid, auth_token)
 
-  # This allows outgoing connections to TwiML application
-  if request.values.get('allowOutgoing') != 'false':
-     capability.allow_client_outgoing(app_sid)
-
-  # This allows incoming connections to client (if specified)
-  client = request.values.get('client')
-  if client != None:
-    capability.allow_client_incoming(client)
-
-  # This returns a token to use with Twilio based on the account and capabilities defined above
-  return capability.generate()
-
-@app.route('/call', methods=['GET', 'POST'])
-def call():
-  """ This method routes calls from/to client                  """
-  """ Rules: 1. From can be either client:name or PSTN number  """
-  """        2. To value specifies target. When call is coming """
-  """           from PSTN, To value is ignored and call is     """
-  """           routed to client named CLIENT                  """
-  resp = twilio.twiml.Response()
-  from_value = request.values.get('From')
-  to = request.values.get('To')
-  if not (from_value and to):
-    resp.say("Invalid request")
-    return str(resp)
-  from_client = from_value.startswith('client')
-  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
-  if not from_client:
-    # PSTN -> client
-    resp.dial(callerId=from_value).client(CLIENT)
-  elif to.startswith("client:"):
-    # client -> client
-    resp.dial(callerId=from_value).client(to[7:])
-  else:
-    # client -> PSTN
-    resp.dial(to, callerId=caller_id)
-  return str(resp)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-  resp = twilio.twiml.Response()
-  resp.say("Welcome to Twilio")
-  return str(resp)
+  return "This is a Twilio API"
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
